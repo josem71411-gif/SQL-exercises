f673d41ba4ec20cbfe1408d8d36e2365ea45d012
josem71411-gif <josem71411@gmail.com>
2026-06-02 14:55:27 -0500

    questions 4,5

--- .idea/basic-queries-northwind.sql
+++ .idea/basic-queries-northwind.sql
@@ -6,3 +6,16 @@ SELECT ProductID, ProductName, UnitPrice FROM Products ORDER BY UnitPrice ASC;
 SELECT ProductID, ProductName, UnitPrice
 FROM Products
 ORDER BY UnitPrice ASC;
+
+
+SELECT ProductID, ProductName, UnitPrice
+FROM Products
+WHERE UnitPrice <= 7.50;
+
+
+SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+FROM Products
+WHERE UnitsInStock >= 100
+ORDER BY UnitPrice DESC;
+
+
